5a6e34ce3e13c93cd5bff74c62bef6d5a1510a39
Scott171 <K.Scott.Hewitson@gmail.com>
2018-12-27 13:22:02 -0700

    started firebase and added css

---
+++ js/form.js
@@ -0,0 +1,14 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB1ot-812RoR14KmbBsCanEc_ubgFzvYXs",
+    authDomain: "cheatday-a9f72.firebaseapp.com",
+    databaseURL: "https://cheatday-a9f72.firebaseio.com",
+    projectId: "cheatday-a9f72",
+    storageBucket: "cheatday-a9f72.appspot.com",
+    messagingSenderId: "436313165419"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
